0a098f86d3242ac76a85803c412a47d0e844829b
Albert Zak <me@albertzak.com>
2017-12-01 19:28:00 +0100

    Post benchmark results to server

--- run.sh
+++ run.sh
@@ -2,7 +2,55 @@
 
 set -e
 
-echo "### Running 'curl https://get.beamup.io/install | /bin/sh'"
-time (curl https://get.beamup.io/install | /bin/sh)
+BENCHMARK_URL=http://163.172.147.195:8080
+benchmark_revision=1
 
-echo "### Success"
+start_time=""
+
+main () {
+  start "install"
+  curl https://get.beamup.io/install | /bin/sh
+  end "install"
+
+  start "full_build_without_cache"
+  BEAMUP_STORE='/root/.beamup-store' beamup build
+  end "full_build_without_cache"
+}
+
+# Private
+
+start () {
+  echo "### Start '$1'"
+  start_time=$(date +%s.%N)
+}
+
+end () {
+  end_time=$(date +%s.%N)
+  diff=$(echo "$end_time - $start_time" | bc)
+  echo "### Finished '$1' took $diff"
+
+  result="revision=$benchmark_revision"
+  result="$result&subject=$BENCHMARK_SUBJECT"
+  result="$result&step=$1"
+  result="$result&time=$diff"
+
+  curl -X POST -d "$result" $BENCHMARK_URL
+}
+
+ensure_env () {
+  if [ -z "$BENCHMARK_SUBJECT" ]; then
+    fail 'Please set $BENCHMARK_SUBJECT'
+  fi
+
+  if [ -z "$BENCHMARK_URL" ]; then
+    fail 'Please set $BENCHMARK_URL'
+  fi
+}
+
+fail () {
+  echo "$1"
+  exit 1
+}
+
+ensure_env
+main
---
+++ semaphore-ci.md
@@ -0,0 +1,5 @@
+## Semaphore CI
+
+Make sure to select *Language: Erlang* and *Erlang version: 20.0* and use this for Job #1:
+
+    PATH=$HOME/.beamup/cli:$PATH BENCHMARK_SUBJECT=Semaphore ./run.sh
