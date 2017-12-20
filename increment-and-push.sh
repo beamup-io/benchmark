0cf73aa60283a3614b7c5ba8f3cd136fb29b4f8b
Albert Zak <me@albertzak.com>
2017-12-20 11:25:38 +0100

    Fix autogenerated commit message

--- increment-and-push.sh
+++ increment-and-push.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-for i in {40..100}; do
+last_revision=$(cat ./benchmark_revision)
+start_revision=$(($last_revision + 1))
+end_revision=$(($start_revision + 60))
+
+for i in $(seq $start_revision $end_revision); do
   echo "Revision $i"
 
   echo "$i" > ./benchmark_revision
 
   git add .
-  git commit -m "Rev $1"
+  git commit -m "Rev $i"
   git push
 
   sleep 180
